e1580e462f524e494f53f986daa6ce2b706921ce
Shubham Patil <shubhamspatilnbr@gmail.com>
2024-06-29 18:06:13 +0530

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <>
-      <div className='w-[108rem] h-[6.875rem] bg-white border-b flex pl-10 pr-6 items-center fixed top-0'>
+      <div className='w-[92rem] h-[6.875rem] bg-white border-b flex pl-10 pr-6 items-center fixed top-0'>
         <div className='w-[104rem] h-[3.8125rem] flex justify-between'>
           <div className='bg-[#E7E7E7] h-[3.8125rem] w-[6.25rem] flex justify-center items-center'>
             <span className='text-[#DC4A2D] text-xl bg-[#E7E7E7] -tracking-normal font-medium'>
@@ -9,10 +9,10 @@ const Navbar = () => {
             </span>
           </div>
 
-          <div className='w-[33.625rem] h-[4.625rem] border rounded-full flex justify-between p-2 relative bottom-2 right-[7.4rem]'>
+          <div className='w-[33.625rem] h-[4.625rem] border rounded-full flex justify-between p-2 relative bottom-1.5'>
             <button className='rounded-full border-2 border-[#fcb4a5] bg-[#dc4a2d] text-white flex justify-center items-center gap-1.5 py-3 px-4'>
               <img src='/jobs.png' alt='jobs' className='w-6 h-6' />
-              <span>Jobs</span>
+              <span className='text-xl tracking-[1px]'>Jobs</span>
             </button>
 
             <button className='flex justify-center items-center py-3 '>
@@ -39,11 +39,18 @@ const Navbar = () => {
             <img
               src='/ghanta.png'
               alt='bell'
-              className='w-8 h-[32px] mt-[3px]'
+              className='w-8 h-[32px] mt-[3px] object-contain'
             />
+            <span className='text-red-600 text-3xl relative bottom-3.5 right-7'>
+              •
+            </span>
             <div className='flex justify-center items-center gap-2'>
               <img src='/atlassian2.png' alt='atlassian' className='w-8 h-8' />
-              <img src='/down.png' alt='down' className='h-2 w-3' />
+              <img
+                src='/down.png'
+                alt='down'
+                className='h-2 w-3 object-contain'
+              />
             </div>
           </div>
         </div>
